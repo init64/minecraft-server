b71a1ef9ac4a4c3a590d60fd2f1d4d3fe01bd31e
webgtx <alx.zolotarov@gmail.com>
2022-12-05 05:49:26 +0000

    Update: smallfixes in start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,5 @@
 #!/bin/sh -e
 
-function installAchievements {
-    if [ ! -d "/server/world/datapacks/BlazeandCave-Advancements" ]; then
-        echo "The installation of new achievements has begun."
-        unzip /server/files/datapacks/BlazeandCaves-Advancements-Pack-1.14.5.zip -d /server/files/datapacks/BlazeandCave-Advancements
-        mv /server/files/datapacks/BlazeandCave-Advancements /server/world/datapacks
-        echo "New achievements have been installed :D"
-    fi
-}
-
 function installMods {
     if [ ! -d "/server/mods" ]; then
         mkdir /server/mods
@@ -23,31 +14,7 @@ function installMods {
     }
 }
 
-function configs {
-    # * MiniMOTD
-    if [ -d "/server/files/MiniMOTD" ]; then
-        rm -rf /server/config/MiniMOTD
-    fi
-    mv -f /server/files/MiniMOTD /server/config
-
-    # * PlasmoVoice
-    if [ -d "/server/files/PlasmoVoice" ]; then
-        rm -rf /server/config/PlasmoVoice
-    fi
-    mv -f /server/files/PlasmoVoice /server/config
-}
-
-if [ ! -d "/server/world/datapacks" ]; then
-    if [ ! -d "/server/world" ]; then
-        mkdir /server/world
-    fi
-
-    mkdir /server/world/datapacks
-fi
-
 installMods
-
-configs
 
 if [ ! -d "/server/config" ]; then
     mkdir /server/config
